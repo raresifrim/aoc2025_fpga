6b64d9ed60b8cd8c8beda55f1efb11fd56220e12
Rares Ifrim <raresifrim@192.168.1.128>
2026-01-05 16:58:01 +0200

    [update]: added solution for day5 puzzle 1

---
+++ src/design/day5.sv
@@ -0,0 +1,88 @@
+module processing_element#(
+    parameter WIDTH=64
+    )(
+        input logic clock,
+        input logic reset,
+        input logic we,
+        input logic [WIDTH-1:0] start_range_in, end_range_in,
+        input logic [WIDTH-1:0] id_in,
+        input logic in_range_in,
+        output logic [WIDTH-1:0] start_range_out, end_range_out,
+        output logic [WIDTH-1:0] id_out,
+        output logic in_range_out
+    );
+
+    logic [WIDTH-1:0] start_reg = '0, end_reg = '0;
+
+    assign start_range_out = start_reg;
+    assign end_range_out = end_reg;
+
+    always_ff@(posedge clock) begin
+        if(reset) begin
+            start_reg <= '0;
+            end_reg <= '0;
+            id_out <= '0;
+            in_range_out <= '0;
+        end
+        else begin
+            if(we) begin
+                start_reg <= start_range_in;
+                end_reg <= end_range_in;
+                in_range_out <= '0;
+            end
+            else begin
+                if((start_reg <= id_in && id_in <= end_reg) || in_range_in) begin
+                    in_range_out <= '1;
+                end
+                else
+                    in_range_out <= '0;
+            end
+            id_out <= id_in;
+        end
+    end
+
+endmodule
+
+module day5_puzzle#(
+    parameter WIDTH=64,
+    parameter DEPTH=183
+    )(
+        input logic clock, reset,
+        input logic load_ranges,
+        input logic [WIDTH-1:0] start_range, end_range,
+        input logic [WIDTH-1:0] id,
+        output logic [31:0] total_fresh
+    );
+
+    generate
+        logic [WIDTH-1:0] start_ranges [DEPTH+1];
+        logic [WIDTH-1:0] end_ranges [DEPTH+1];
+        logic [WIDTH-1:0] ids [DEPTH+1];
+        logic [DEPTH:0] in_range;
+        assign start_ranges[0] = start_range;
+        assign end_ranges[0] = end_range;
+        assign ids[0] = id;
+        assign in_range[0] = '0;
+
+        genvar i;
+        for(i=0;i<DEPTH;i++) begin
+            processing_element#(.WIDTH(WIDTH)) processing_element_inst(
+                .clock(clock),
+                .reset(reset),
+                .we(load_ranges),
+                .start_range_in(start_ranges[i]),
+                .end_range_in(end_ranges[i]),
+                .start_range_out(start_ranges[i+1]),
+                .end_range_out(end_ranges[i+1]),
+                .id_in(ids[i]),
+                .id_out(ids[i+1]),
+                .in_range_in(in_range[i]),
+                .in_range_out(in_range[i+1])
+                );
+        end
+
+        counter #(.W(32)) counter_inst(.clock(clock), .reset(reset), .load('0), .cnt_up(in_range[DEPTH] & ~load_ranges), .cnt_in('0), .cnt_out(total_fresh));
+
+    endgenerate
+
+endmodule
---
+++ src/sim/day5_tb.sv
@@ -0,0 +1,81 @@
+module day5_puzzle_tb();
+
+    localparam int WIDTH = 64;
+    localparam int DEPTH = 183;
+    localparam int CLK_PERIOD = 10;
+    localparam int TOTAL_RANGES = 183;
+    localparam int TOTAL_IDS = 1000;
+
+    logic clock = '0, reset = '1, load_ranges='0;
+    logic [WIDTH-1:0] start_range, end_range;
+    logic [WIDTH-1:0] id;
+    logic [31:0] total_fresh;
+
+    day5_puzzle#(.DEPTH(DEPTH),.WIDTH(WIDTH)) day5_puzzle_dut(.*);
+
+    int fd;
+    string line;
+    int range_or_ids=0;
+    logic [WIDTH-1:0] start_ranges [TOTAL_RANGES];
+    logic [WIDTH-1:0] end_ranges [TOTAL_RANGES];
+    logic [WIDTH-1:0] ids [TOTAL_IDS];
+
+    initial begin
+        $dumpfile("dump.vcd");
+        $dumpvars(0, day9_puzzle1_tb);
+        forever begin
+            #(CLK_PERIOD/2) clock = ~clock;
+        end
+    end
+
+    //read ranges and ids
+    initial begin
+        // Open the file in read mode
+        fd = $fopen("../../inputs/day5_puzzle1_in.txt", "r");
+        if (fd == 0) begin
+            $display("Error: input.txt file handle was NULL. Check file path.");
+            $finish;
+        end
+
+        $display("Reading data from input.txt...");
+        for(int i=0;i<TOTAL_RANGES;i++) begin
+            $fgets (line, fd);
+            $sscanf(line, "%d:%d", start_ranges[i], end_ranges[i]);
+        end
+        $fgets (line, fd);//skip over empty line
+        for(int i=0;i<TOTAL_IDS;i++) begin
+            $fgets (line, fd);
+            $sscanf(line, "%d", ids[i]);
+            //$display("Current id:%d",ids[i]);
+        end
+    end
+
+    initial begin
+        wait(clock == 1);
+        wait(clock == 0); //perform a complete clock cycle
+        reset = '0;
+
+        load_ranges = '1;
+        for(int i=0;i<TOTAL_RANGES;i++) begin
+            wait(clock == 0);
+            start_range = start_ranges[i];
+            end_range = end_ranges[i];
+            wait(clock == 1);
+        end
+        wait(clock == 0);
+        load_ranges = '0;
+
+        for(int i=0;i<TOTAL_IDS;i++) begin
+            wait(clock == 0);
+            id = ids[i];
+            wait(clock == 1);
+        end
+        wait(clock == 0); //wait one more clock cycle for last value
+        id = '0;
+        wait(clock == 1);
+
+        #((DEPTH+1)*CLK_PERIOD);
+        $display("Total fresh indredients:%d", total_fresh);
+        $finish;
+    end
+endmodule
