4e88d0cd790eeb569af13c2b072d4fa084c4f89d
raresifrim <ifrimrares@yahoo.com>
2026-01-13 16:37:30 +0200

    [minor update]: split day1&2 testbenches per parts

--- src/sim/day1_tb.sv
+++ src/sim/day1_tb.sv
@@ -1,4 +1,91 @@
-module day1_puzzle_tb();
+module day1_puzzle1_tb();
+
+    localparam int PUZZLE = 1;
+    localparam int WIDTH = 16;
+
+    logic clock = 1'b0;
+    logic reset = 1'b1;
+    logic init = 1'b0, valid = 1'b0, rotation = 1'b0;
+    logic ready;
+    logic [WIDTH-1:0] rotate_amount, max_number, password;
+
+    int fd, num;
+    string line;
+    byte direction;
+
+    day1_puzzle #(.WIDTH(WIDTH), .PUZZLE(PUZZLE)) dut(.*);
+
+    always #5 clock = ~clock;
+
+    initial begin: resetBehaviour
+        #0 reset = 1'b0;
+        wait(clock==1);
+        reset = 1'b1;
+        wait(clock==0);
+        reset = 1'b0;
+    end
+
+    initial begin
+
+        $dumpfile("dump.vcd");
+        $dumpvars(0, day1_puzzle1_tb);
+        //$monitor ("[$monitor] time=%0t password = %d", $time, password);
+
+        #10;
+
+        wait(clock==1 && reset==0);
+        init = 1'b1;
+        rotate_amount = 50;
+        max_number = 99;
+        wait(clock==0);
+        init = 1'b0;
+
+        // Open the file in read mode
+        fd = $fopen("../../inputs/day1_puzzle1_in.txt", "r");
+        //fd = $fopen("../../inputs/day1_test.txt", "r");
+        if (fd == 0) begin
+            $display("Error: input.txt file handle was NULL. Check file path.");
+            $finish;
+        end
+
+        $display("Reading data from input.txt...");
+
+        while (!$feof(fd)) begin
+            //if (dut.ready)
+            //   $write("[%d,%d]", dut.number_ff, dut.password_ff);
+            wait(clock==1);
+            if (!ready) begin
+                valid = 1'b1;
+                wait(clock==0);
+            end
+            else begin
+                $fgets (line, fd);
+                $sscanf(line, "%c%d", direction, num);
+                //we sync each new input at beggining of a clock cycle
+                rotate_amount = num;
+                rotation = (direction == "R");
+                valid = 1'b1;
+                //$display("Current rotation: %d, Current direction: %d", num, rotation);
+                wait(clock==0);
+                //if (dut.number_ff == 0)
+                    //$display("Accumulator equal to zero!");
+            end
+        end
+
+        // Close the file
+        $fclose(fd);
+
+        wait(clock==1);
+        valid = 1'b0;
+        $display("Finished reading file. Password is %d", password);
+        #10
+        $finish;
+    end
+
+endmodule
+
+
+module day1_puzzle2_tb();
 
     localparam int PUZZLE = 2;
     localparam int WIDTH = 16;
@@ -28,7 +115,7 @@ module day1_puzzle_tb();
     initial begin
 
         $dumpfile("dump.vcd");
-        $dumpvars(0, day1_puzzle1_tb);
+        $dumpvars(0, day1_puzzle2_tb);
         //$monitor ("[$monitor] time=%0t password = %d", $time, password);
 
         #10;
--- src/sim/day2_tb.sv
+++ src/sim/day2_tb.sv
@@ -1,4 +1,73 @@
-module day2_puzzle_tb();
+module day2_puzzle1_tb();
+
+    localparam W=48; //large enough for the provided ranges, while also being able to map to DSPs
+    localparam NUM_UNITS=38; //we instantiate a unit for each range
+    localparam CLK_PERIOD = 10;
+    localparam PUZZLE = 1;
+    localparam DSP_UNITS = (NUM_UNITS / 2) + (NUM_UNITS % 2);
+
+    logic clock = 0, reset = 1,load = 0, en = 0;
+    logic [W-1:0] start_id [0:NUM_UNITS-1]; 
+    logic [W-1:0] end_id [0:NUM_UNITS-1];
+    logic [W-1:0] id_sum;
+    logic done;
+
+    day2_puzzle #(.W(W), .NUM_UNITS(NUM_UNITS), .PUZZLE(PUZZLE)) day2_puzzle_dut(.*);
+
+    int fd, range_idx=0;
+    bit [W-1:0] a ,b;
+    string line; //compatible with both vivado and iverilog
+
+    initial begin
+        $dumpfile("dump.vcd");
+        $dumpvars(0, day2_puzzle1_tb);
+        forever begin
+            #(CLK_PERIOD/2) clock = ~clock;
+        end
+    end
+
+    initial begin
+        wait(clock == 1);
+        wait(clock == 0); //perform a complete clock cycle
+        reset = '0;
+        load = 1'b1;
+
+        // Open the file in read mode
+        fd = $fopen("../../inputs/day2_puzzle1_in.txt", "r");
+        //fd = $fopen("../../inputs/day2_test.txt", "r");
+        if (fd == 0) begin
+            $display("Error: input.txt file handle was NULL. Check file path.");
+            $finish;
+        end
+
+        $display("Reading data from input.txt...");
+        //test single range...NUM_UNITS must be set to 1
+        //start_id[0] = 527473787;
+        //end_id[0] = 527596071;
+        //test from file
+        while (!$feof(fd)) begin
+            $fgets (line, fd);
+            $sscanf(line, "%d:%d", a, b); //had to modify the range separator to work in verilator
+            start_id[range_idx] = a;
+            end_id[range_idx] = b;
+            $display("%d -> %d", start_id[range_idx], end_id[range_idx]);
+            range_idx += 1;
+        end
+
+        wait(clock == 1);
+        wait(clock == 0);
+        load = 0;
+        en = 1;
+
+        wait(done == 1);
+        #((DSP_UNITS+1)*CLK_PERIOD); //worst case wait for all DSPs to propagate their values
+       $display("Result is: %d", id_sum);
+        $finish;
+    end
+
+endmodule
+
+module day2_puzzle2_tb();
 
     localparam W=48; //large enough for the provided ranges, while also being able to map to DSPs
     localparam NUM_UNITS=38; //we instantiate a unit for each range
@@ -20,7 +89,7 @@ module day2_puzzle_tb();
 
     initial begin
         $dumpfile("dump.vcd");
-        $dumpvars(0, day2_puzzle_tb);
+        $dumpvars(0, day2_puzzle2_tb);
         forever begin
             #(CLK_PERIOD/2) clock = ~clock;
         end
